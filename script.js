43c47f43e9e0aba3a1540331d0494aba098b1543
cori0lis <67338651+cori0lis@users.noreply.github.com>
2020-07-23 16:26:40 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+/* 
+horizontal
+    IF
+        top-left,
+        top-middle
+        top-right 
+        = rebel 
+        
+    THEN  rebel win
+    
+    IF
+        ML
+        MM
+        MR
+        = rebel
+    
+    THEN rebel win
+    
+    IF
+        BL
+        BM
+        BR
+        =rebel
+    
+    THEN rebel win
+    
+vertical
+    IF
+        TL
+        ML
+        BL
+        = rebel
+        
+    THEN rebel win
+    
+    IF
+        TM
+        MM
+        BM
+        = rebel
+        
+    THEN rebel win
+    
+    IF
+        TR
+        MR
+        BR
+        = rebel
+        
+    THEN rebel win
+    
+diagonal
+    IF
+        TL
+        MM
+        BR
+        = rebel
+    
+    THEN rebel win
+    
+    IF
+        TR
+        MM
+        BL
+        = rebel
+        
+    THEN rebel win*/
